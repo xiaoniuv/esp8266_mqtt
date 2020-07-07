88707fa4873f1d7c864f69a5b4f4552fdb76ebeb
xiaoniu <1572763248@qq.com>
2020-07-07 17:12:46 +0800

    Mqtt

---
+++ ESP8266_NONOS_SDK-3.0 - mqtt -control/app/include/mqtt/MQ.h
@@ -0,0 +1,17 @@
+/*
+ * MQ.h
+ *
+ *  Created on: 2019Äê3ÔÂ26ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef APP_INCLUDE_MQTT_MQ_H_
+#define APP_INCLUDE_MQTT_MQ_H_
+
+
+int flag;
+char temp_data[10];
+char humi_data[10];
+
+
+#endif /* APP_INCLUDE_MQTT_MQ_H_ */
---
+++ ESP8266_NONOS_SDK-3.0 - mqtt -control/app/mqtt/include/MQ.h
@@ -0,0 +1,15 @@
+/*
+ * MQ.h
+ *
+ *  Created on: 2019Äê3ÔÂ29ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef APP_MQTT_INCLUDE_MQ_H_
+#define APP_MQTT_INCLUDE_MQ_H_
+
+int flag;
+
+
+
+#endif /* APP_MQTT_INCLUDE_MQ_H_ */
